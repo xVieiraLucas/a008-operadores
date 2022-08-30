2e5d1437a28feef3de8f9496f0b12e446061e1a1
xVieiraLucas <111319507+xVieiraLucas@users.noreply.github.com>
2022-08-30 13:50:10 -0300

    correção

--- exercicio3/script.js
+++ exercicio3/script.js
@@ -5,8 +5,8 @@ const operacaoB = 5 === 5 || 5 === "5";
 console.log("5 é igual a 5 ou é igual a “5” ?", operacaoB)
 
 const operacaoC = 20 > 50
-console.log("Vinte é maior que cinquenta ?", !operacaoC)
+console.log("Negação de (vinte é maior que cinquenta) :", !operacaoC)
 
 const operacaoD = 20 > 50 || 50 > 60
-console.log("vinte é maior que cinquenta OU cinquenta é maior que sessenta ?", !operacaoD)
+console.log("Negação de (vinte é maior que cinquenta OU cinquenta é maior que sessenta):", !operacaoD)
 
